659a5caddd555dde287304b6a7f784b3117198d5
Petr Ševčík <petr.sevcik@eox.at>
2020-02-26 11:20:13 +0100

    dev commit

--- function.py
+++ function.py
@@ -1,6 +1,11 @@
 
-def function(a,b):
+def function(a, b):
+    # None of my bussiness lives here!
 
     c = a + b
 
     return c
+
+
+def petr_stuff(fancy=1):
+    return [e+1 for e in range(10)]
